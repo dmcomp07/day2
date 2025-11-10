30cb5a3c7617c714b64e53f8f7fed825bba55057
Nav Sagar <97079719+dmcomp07@users.noreply.github.com>
2025-11-10 09:00:46 +0530

    Update Helloworld to print a motivational quote

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("सपनों की उड़ान वही भरते हैं, जो डर को पीछे छोड़ देते हैं। हर कदम पर मौका है, बस हिम्मत से आगे बढ़ने की देर है।");
     }
 }
