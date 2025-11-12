ad4aad9909cb85a26f888b02a922ec023a46805c
Nav Sagar <97079719+dmcomp07@users.noreply.github.com>
2025-11-12 09:08:37 +0530

    Change motivational quote in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("सपनों की उड़ान वही भरते हैं, जो डर को पीछे छोड़ देते हैं। हर कदम पर मौका है, बस हिम्मत से आगे बढ़ने की देर है।");
+        System.out.println("Making one person smile can change the world – maybe not the whole world, but their world");
     }
 }
+
